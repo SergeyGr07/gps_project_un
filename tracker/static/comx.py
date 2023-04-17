e655622fb053cc3607a19f129e6fcda86cb57cd6
SergeyGr07 <anonymer356@gmail.com>
2023-04-17 17:23:42 +0800

    new_one

--- tracker/static/comx.py
+++ tracker/static/comx.py
@@ -2,7 +2,7 @@ import serial
 
 # Открываем последовательный порт
 ser = serial.Serial('COM4', 9600)
-
+print("Processing...")
 # Открываем файл для записи данных
 with open('D:/coding/gps_project/data.txt', 'w') as f:
     # Бесконечный цикл для чтения данных
